47950259f661880bae1678620e6fa9f164888a77
Chandan8585 <chandanwingshr@gmail.com>
2023-06-17 23:59:59 +0530

    checking

--- dom_manipulation/script.js
+++ dom_manipulation/script.js
@@ -1,6 +1,10 @@
 let start = document.getElementById("start-button");
 let count = document.getElementById("count");
+let stopButton = document.getElementById("stop");
 let point = 0;
+stopButton.addEventListener("click", ()=> {
+         stopButton.classList.toggle("btn-red");  
+})
 count.addEventListener("click", ()=>{
     point++;
    
