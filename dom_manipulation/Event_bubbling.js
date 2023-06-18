84436654654cb87e7f31d062e5fa4395bc18377d
Chandan8585 <chandanwingshr@gmail.com>
2023-06-18 20:04:17 +0530

    Event bubbling done

---
+++ dom_manipulation/Event_bubbling.js
@@ -0,0 +1,26 @@
+let form = document.querySelector(".form");
+let div = document.querySelector(".div");
+let para = document.querySelector(".para");
+
+// form.addEventListener("click", ()=> {
+//     alert("form Tag");
+// })
+
+// div.addEventListener("click", ()=> {
+//     alert("div Tag");
+// })
+// para.addEventListener("click", ()=> {
+//     alert("para Tag");
+// })
+
+
+form.addEventListener("click", ()=> {
+    alert("form Tag");
+}, )
+
+div.addEventListener("click", ()=> {
+    alert("div Tag");
+}, true)
+para.addEventListener("click", ()=> {
+    alert("para Tag");
+},true)
