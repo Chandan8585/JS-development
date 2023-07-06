124f906472a6e84a0b4445bd0e318d45c8958e9d
Chandan8585 <chandanwingshr@gmail.com>
2023-07-06 23:50:06 +0530

    Added Feature

---
+++ Ecom-app/index.js
@@ -0,0 +1,8 @@
+import { products } from "./db/product.js";
+console.log(products);
+
+const productContainer = document.getElementById("products");
+
+for(let product of products){
+    const cardContainer = document
+}
