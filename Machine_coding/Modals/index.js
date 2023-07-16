184158e80f8f292ce1a72faae22caba750d0e5c3
Chandan8585 <chandanwingshr@gmail.com>
2023-07-16 23:33:09 +0530

    Modals Added

--- Machine_coding/Modals/index.js
+++ Machine_coding/Modals/index.js
@@ -1,28 +1,12 @@
 
-// closeBtn.addEventListener("click", ()=> {
-//         //      if(event.target.classList.contains(closeBtn)){
-//          para.classList.toggle("modalClose");
-//         //  closeBtn.innerHTML = "show Button"
-//         //    }
-//         // else if(event.target.classList.contains(showBtn)){
-//         //     para.classList.remove("modalClose");
-//         // }
-   
-// })
+
 const showBtn = document.querySelector(".show");
 const closeBtn = document.querySelector(".close");
 const para = document.querySelector(".para");
 const body = document.querySelector("body");
 const lightController = document.querySelector(".light");
 
-// lightController.addEventListener("click", (e)=> {
-//     if(e.target.classList.contains("day")){
-//           body.classList.add("dayLight");
-//     }
-//    else if(e.target.classList.contains("night")){
-//         body.classList.add("nightLight");
-//   }
-// })
+
 
 
 lightController.addEventListener("click", (e) => {
